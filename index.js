aa9f48730e524598998917310b1545942718eb03
David Hsu <dvdhsu@gmail.com>
2017-03-23 06:08:18 -0700

    Babel to ES5

--- index.js
+++ index.js
@@ -1,115 +1,114 @@
-'use strict'
-const esprima = require('esprima')
-const estraverse = require('estraverse')
+'use strict';
 
-function createsNewScope (node) {
-  return node.type === 'FunctionDeclaration' ||
-    node.type === 'FunctionExpression' ||
-    node.type === 'Program'
+var esprima = require('esprima');
+var estraverse = require('estraverse');
+
+function createsNewScope(node) {
+  return node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression' || node.type === 'Program';
 }
 
-module.exports = (sourceCode) => {
-  const ast = esprima.parse(sourceCode, {
+module.exports = function (sourceCode) {
+  var ast = esprima.parse(sourceCode, {
     jsx: true,
     tolerant: true
-  })
-  const scopeChain = []
-  let identifiers = []
-  let lastFoundIdentifier = null;
-  const undefinedIdentifiers = new Set()
+  });
+  var scopeChain = [];
+  var identifiers = [];
+  var lastFoundIdentifier = null;
+  var undefinedIdentifiers = new Set();
 
-  function enter (node, parent) {
+  function enter(node, parent) {
     // clear lastFoundIdentifier
     if (parent && parent.type !== 'MemberExpression') {
-      lastFoundIdentifier = null
+      lastFoundIdentifier = null;
     }
 
     if (createsNewScope(node)) {
-      scopeChain.push([])
+      scopeChain.push([]);
     }
     if (node.type === 'VariableDeclarator') {
-      const currentScope = scopeChain[scopeChain.length - 1]
+      var currentScope = scopeChain[scopeChain.length - 1];
       if (node.id.name) {
-        currentScope.push(node.id.name)
+        currentScope.push(node.id.name);
       } else {
-        const pushDesctructuredToScope = (properties) => {
-          properties.forEach((prop) => {
+        var pushDesctructuredToScope = function pushDesctructuredToScope(properties) {
+          properties.forEach(function (prop) {
             if (prop.value.name) {
-              currentScope.push(prop.value.name)
-            } else { // deeper into the destructuring object tree
-              pushDesctructuredToScope(prop.value.properties)
+              currentScope.push(prop.value.name);
+            } else {
+              // deeper into the destructuring object tree
+              pushDesctructuredToScope(prop.value.properties);
             }
-          })
-        }
-        pushDesctructuredToScope(node.id.properties)
+          });
+        };
+        pushDesctructuredToScope(node.id.properties);
       }
       if (node.id.type === 'ObjectPattern') {
-        return estraverse.VisitorOption.Skip
+        return estraverse.VisitorOption.Skip;
       }
     }
     if (parent && parent.type === 'MemberExpression') {
-      if ((parent.object && parent.object.name === lastFoundIdentifier) ||
-        parent.object.object && parent.object.property.name === lastFoundIdentifier) {
-        let lastConcat = identifiers[identifiers.length - 1]
-        lastConcat += `.${node.name}`
-        identifiers[identifiers.length - 1] = lastConcat
-        lastFoundIdentifier = node.name
+      if (parent.object && parent.object.name === lastFoundIdentifier || parent.object.object && parent.object.property.name === lastFoundIdentifier) {
+        var lastConcat = identifiers[identifiers.length - 1];
+        lastConcat += '.' + node.name;
+        identifiers[identifiers.length - 1] = lastConcat;
+        lastFoundIdentifier = node.name;
       }
       if (node.name && parent.object.name === node.name) {
-        lastFoundIdentifier = node.name
-        identifiers.push(node.name)
+        lastFoundIdentifier = node.name;
+        identifiers.push(node.name);
       }
     } else {
       if (node.type === 'Identifier') {
         if (parent.type !== 'VariableDeclarator') {
           if (parent.type === 'Property') {
             if (parent.key === node) {
-              return
+              return;
             }
           }
-          identifiers.push(node.name)
+          identifiers.push(node.name);
         }
       }
     }
   }
 
-  function leave (node) {
+  function leave(node) {
     if (node.type === 'CallExpression') {
       // prop.value.funcCall
-      const lastId = identifiers[identifiers.length - 1]
+      var lastId = identifiers[identifiers.length - 1];
       // prop.value
-      identifiers[identifiers.length - 1] = lastId.split('.').slice(0, lastId.split(',').length - 2).join('.')
+      identifiers[identifiers.length - 1] = lastId.split('.').slice(0, lastId.split(',').length - 2).join('.');
     }
 
     if (createsNewScope(node)) {
-      checkForLeaks(identifiers, scopeChain)
-      scopeChain.pop()
-      identifiers = []
+      checkForLeaks(identifiers, scopeChain);
+      scopeChain.pop();
+      identifiers = [];
     }
   }
 
-  function isVarDefined (varname, scopeChain) {
+  function isVarDefined(varname, scopeChain) {
     for (var i = 0; i < scopeChain.length; i++) {
-      const scope = scopeChain[i]
+      var scope = scopeChain[i];
       // console.log('scopeChain: ', scopeChain);
       if (scope.indexOf(varname) !== -1) {
-        return true
+        return true;
       }
     }
-    return false
+    return false;
   }
 
-  function checkForLeaks (identifiers, scopeChain) {
-    identifiers.forEach((identifier) => {
+  function checkForLeaks(identifiers, scopeChain) {
+    identifiers.forEach(function (identifier) {
       if (!isVarDefined(identifier, scopeChain)) {
-        undefinedIdentifiers.add(identifier)
+        undefinedIdentifiers.add(identifier);
       }
-    })
+    });
   }
 
   estraverse.traverse(ast, {
     enter: enter,
     leave: leave
-  })
-  return Array.from(undefinedIdentifiers)
-}
+  });
+  return Array.from(undefinedIdentifiers);
+};
